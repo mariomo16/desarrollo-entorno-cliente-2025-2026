81aaeff5f42f1b8ab7cc2a48e527e23cba1944ec
mariomo16 <mariomo16.cieza@gmail.com>
2026-01-15 12:19:40 +0100

    clase a medias

--- Angular/new-app/src/app/components/zapatillas-component/zapatillas-component.ts
+++ Angular/new-app/src/app/components/zapatillas-component/zapatillas-component.ts
@@ -1,15 +1,27 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
 	selector: "app-zapatillas-component",
-	imports: [],
+	imports: [FormsModule],
 	templateUrl: "./zapatillas-component.html",
 	styleUrl: "./zapatillas-component.css",
 })
 export class ZapatillasComponent {
 	public titulo: string;
+	public zapatillas: Array<Zapatilla>;
+	public marcas: String[];
+	public color: string;
+	public mi_marca: string;
 
 	constructor() {
-		this.titulo = "Componente de Zapatillas";
+		this.titulo = "Componente Zapatillas";
+		this.zapatillas = [
+			new Zapatilla("Reebook Classic", "Reebook", "Blanco", 80, true),
+			new Zapatilla("Nike Runner MD", "Nike", "Negro", 60, true),
+			new Zapatilla("Adidas Yezzy", "Adidas", "Gris", 180, false),
+		];
+		this.color = "yellow";
+		this.mi_marca = "";
 	}
 }
