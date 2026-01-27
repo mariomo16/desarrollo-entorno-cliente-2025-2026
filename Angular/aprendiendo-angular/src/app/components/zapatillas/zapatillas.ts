a3d95f7388361d4bc20c6db8c488777149d38c9b
mariomo16 <mariomo16.cieza@gmail.com>
2026-01-27 18:36:05 +0100

    13.1

--- Angular/aprendiendo-angular/src/app/components/zapatillas/zapatillas.ts
+++ Angular/aprendiendo-angular/src/app/components/zapatillas/zapatillas.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { RouterLink } from "@angular/router";
 import { Zapatilla } from "../../models/Zapatilla";
 
 @Component({
 	selector: "app-zapatillas",
-	imports: [RouterLink],
+	imports: [RouterLink, FormsModule],
 	templateUrl: "./zapatillas.html",
 	styleUrl: "./zapatillas.css",
 })
@@ -24,10 +25,10 @@ export class Zapatillas {
 		];
 		this.marcas = [];
 		this.color = "yellow";
-		this.miMarca = "";
+		this.miMarca = "Champion";
 	}
 
-	getMarcas() {
+    getMarcas() {
 		this.zapatillas.forEach((zapatilla, index) => {
 			if (this.marcas.indexOf(zapatilla.marca) < 0) {
 				this.marcas.push(zapatilla.marca);
@@ -38,6 +39,14 @@ export class Zapatillas {
 
 		console.log(this.marcas);
 	}
+
+    getMarca() {
+        alert(this.miMarca);
+    }
+
+    addMarca() {
+        this.marcas.push(this.miMarca);
+    }
 
 	ngOnInit() {
 		console.log(this.zapatillas);
